b6af1405c077385428cb082750ac0697ac41415b
riku salkia <riksa@iki.fi>
2013-01-11 20:37:01 +0200

    initial commit

---
+++ src/clj_waltawa/core.clj
@@ -0,0 +1,1 @@
+(ns clj-waltawa.core)
---
+++ test/clj_waltawa/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-waltawa.test.core
+  (:use [clj-waltawa.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
