619e852642354766edcc83afe45dc1c4a2f46908
riku salkia <riksa@iki.fi>
2013-01-13 16:41:15 +0200

    docs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-waltawa "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "AI for http://www.ohjelmointiputka.net/kilpa.php?tunnus=valtapeli"
   :main clj-waltawa.core
   :dependencies [[org.clojure/clojure "1.3.0"]])
